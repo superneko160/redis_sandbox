1b0b7678c4f55209ea7a24b48d14ac9d9b9ec0fe
sneko <supernekocat31@gmail.com>
2025-07-19 22:44:38 +0900

    first commit

---
+++ readme.md
@@ -0,0 +1,78 @@
+# Redisと連携
+
+## 0. Rustインストール
+
+[公式サイト](https://www.rust-lang.org/ja/tools/install)のとおりにインストール
+
+ここではWSL上でRustとRedisの連携テストを行う
+
+```sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+一度ターミナルから `exit` する必要有
+
+## 1. Redisのインストール
+
+### Debian・Ubuntu
+
+```sh
+sudo apt update
+sudo apt install redis-server
+```
+
+```sudo apt install redis```でもOK
+
+
+### ArchLinux
+
+```sh
+sudo pacman -Sy
+sudo pacman -S redis
+```
+
+> [!NOTE]
+> 正常にインストールできない場合、redisをフォークして作成された `valkey` へ移行された可能性が高いので、 `valkey` をインストールする
+>
+> `sudo pacman -S valkey`
+
+
+## 2. Redis起動
+
+```sh
+systemctl start redis-server
+```
+
+or
+
+```sh
+sytemctl start valkey
+```
+
+### 正常に動作しているかチェック
+
+```sh
+redis-cli ping
+```
+`PONG`と返ってきたら動作している
+
+## 3. 実行
+
+### データ登録
+
+```sh
+cargo run add <key> <value>
+cargo run add A100 Alice
+```
+
+### データ取得
+
+```sh
+cargo run get <key>
+cargo run get A100
+```
+
+## 4. 使用クレート
+
+- [redis - Rust](https://docs.rs/redis/latest/redis/)
+
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+extern crate redis;
+use redis::Commands;
+use std::env;
+
+// Redisと接続
+fn connection_handling() -> redis::RedisResult<redis::Connection> {
+    let client = redis::Client::open("redis://127.0.0.1")?;
+    let con = client.get_connection()?;
+    Ok(con)
+}
+
+// Redisにデータを挿入
+fn set_to_redis(mut con: redis::Connection, key: String, value: String) -> redis::RedisResult<()> {
+    let _: () = con.set(&key, &value)?;
+    Ok(())
+}
+
+// Redisからデータを取得
+fn get_from_redis(mut con: redis::Connection, key: String) -> redis::RedisResult<String> {
+   Ok(con.get(&key)?)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("usage:");
+        eprintln!("  cargo run add <key> <value>");
+        eprintln!("  cargo run get <key>");
+        return;
+    }
+
+    let command = &args[1];
+
+    match connection_handling() {
+        Ok(con) => {
+            match command.as_str() {
+                "add" => {
+                    if args.len() != 4 {
+                        eprintln!("usage: cargo run add <key> <value>");
+                        return;
+                    }
+                    let key = args[2].clone();
+                    let value = args[3].clone();
+                    match set_to_redis(con, key.clone(), value.clone()) {
+                        Ok(..) => {
+                            println!("データ登録完了：キー{}, 値={}", key, value);
+                        }
+                        Err(e) => {
+                            println!("登録失敗：{}", e);
+                        }
+                    }
+                }
+                "get" => {
+                    if args.len() != 3 {
+                        eprintln!("usage: cargo run get <key> <value>");
+                        return;
+                    }
+                    let key = args[2].clone();
+                    match get_from_redis(con, key.clone()) {
+                        Ok(value) => {
+                            println!("データ取得完了：キー{}, 値={}", key, value);
+                        }
+                        Err(e) => {
+                            println!("取得失敗：{}", e);
+                        }
+                    }
+                }
+                _ => {
+                    eprintln!("無効なコマンドです。'add' または 'get' を使用してください。");
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Redis接続エラー: {}", e);
+        }
+    }
+}
